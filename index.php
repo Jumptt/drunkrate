25bf5fc3313d5bfda59ac30230a1601751dc19c1
Jumptt <64533765+Jumptt@users.noreply.github.com>
2020-05-25 10:30:36 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>bot first</title>
+    <link rel="stylesheet" href="botty.css">
+  </head>
+  <body>
+    <h1>あなたの酔いを判定します</h1>
+    <form class="" action="get.php" method="post">
+      <p>体重を入力してクダサイ</p>
+      <input type="number" name="weight" value="">
+      <br>
+      <p>飲んだお酒の度数を入力してください</p>
+      <input type="number" name="density" value="">
+      <br>
+      <p>飲んだおしゃけの量を入力してください</p>
+      <input type="number" name="quant" value="">
+      <input type="submit" name="" value="送信">
+    </form>
+
+
+  </body>
+</html>
